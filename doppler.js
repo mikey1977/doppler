272bec9a5102ea177733b0fdc37c2448877d0e14
mikey1977 <mmi@umich.edu>
2016-06-01 03:36:08 -0700

    safety save

--- doppler.js
+++ doppler.js
@@ -1,7 +1,11 @@
 function shiftAnswer() {
   var correct = document.getElementById("shift").value.toLowerCase();
   if (correct === "blue") {
-    document.getElementById("reply").innerHTML = "That's Right!  As the light source moves closer to the observer, the wavelength is compressed resulting in that blue shift!  Conversely, if the source was moving away from the observer, the wavelength would seem elongated, and a shift to the red would be observed.  Keep in mind, the wavelength at the source doesn't actually change!";
+    document.getElementById("reply").innerHTML = "That's Right!  As the light source moves closer to the observer, <br>\
+    the wavelength is compressed resulting in that blue shift!  <br>\
+    Conversely, if the source was moving away from the observer, <br>\
+    the wavelength would seem elongated, and a shift to the red would be observed.  <br>\
+    Keep in mind, the wavelength at the source doesn't actually change!";
   } else {
     document.getElementById("reply").innerHTML = "Try again.";
 
