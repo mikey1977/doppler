e7936d0c0323695bc0f5662ec24abfa886ef0928
mikey1977 <mmi@umich.edu>
2016-06-01 03:15:14 -0700

    red shift animation

--- doppler.js
+++ doppler.js
@@ -1,7 +1,7 @@
 function shiftAnswer() {
   var correct = document.getElementById("shift").value.toLowerCase();
   if (correct === "blue") {
-    document.getElementById("reply").innerHTML = "That's Right!  Of course, the wavelength at the source doesn't change!";
+    document.getElementById("reply").innerHTML = "That's Right!  As the light source moves closer to the observer, the wavelength is compressed resulting in that blue shift!  Conversely, if the source was moving away from the observer, the wavelength would seem elongated, and a shift to the red would be observed.  Keep in mind, the wavelength at the source doesn't actually change!";
   } else {
     document.getElementById("reply").innerHTML = "Try again.";
 
